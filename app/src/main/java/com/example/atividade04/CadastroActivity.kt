70210556cd3aa25c4c3fa6d51aa299980ad924c2
jhondutrati <78240326+jhondutrati@users.noreply.github.com>
2022-06-20 21:23:51 -0300

    atualizações teste

--- app/src/main/java/com/example/atividade04/CadastroActivity.kt
+++ app/src/main/java/com/example/atividade04/CadastroActivity.kt
@@ -1,12 +1,19 @@
 package com.example.atividade04
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class CadastroActivity : AppCompatActivity(){
+class CadastroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_cadastro)
+
+        setTitle("Novo Contatinho")
+
+        val botaoSalvar = findViewById<Button>(R.id.btnSalvar)
+        botaoSalvar.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/atividade04/ListaActivity.kt
+++ app/src/main/java/com/example/atividade04/ListaActivity.kt
@@ -1,13 +1,31 @@
 package com.example.atividade04
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class ListaActivity : AppCompatActivity() {
+class ListaActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTitle("Pokedex")
         setContentView(R.layout.layout_lista)
-        setSupportActionBar(findViewById(R.id.toolbar))
+
+        setTitle("@Contatinhos")
+
+        val contatinhos = arrayOf("Contatinho 1", "Contatinho 2", "Contatinho 3")
+        var adaptador = ArrayAdapter<String>(this,
+            android.R.layout.simple_list_item_1, contatinhos)
+
+        val lista = findViewById<ListView>(R.id.lstLista)
+        lista.adapter = adaptador
+
+        val botao = findViewById<FloatingActionButton>(R.id.fabAdicionar)
+        botao.setOnClickListener {
+            val intencao = Intent(applicationContext, CadastroActivity::class.java)
+            startActivity(intencao)
+        }
     }
+
 }
